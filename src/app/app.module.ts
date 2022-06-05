4b13450192e25c768b798cd4ba7358b9e0e8b52e
Augusto Rodrigues de Oliveira Sardi <augusto.roct@gmail.com>
2022-06-05 13:39:45 -0300

    added kaggle dataset perfil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSelectModule} from '@angular/material/select';
+import {MatTableModule} from '@angular/material/table';
 import { ModelComponent } from './model/model.component';
-import {MatTableModule} from '@angular/material/table'
+import { PerfilComponent } from './model/perfil/perfil.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ModelComponent
+    ModelComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
